c4ba2ac34c8b6dcfe0021fbe50710931d1da9571
Shubham Joshi <indsjos@gmail.com>
2026-03-18 10:20:48 +0530

    exception handling

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+import sys
 from src.Telco_customer_churn.logger import logging
-
+from src.Telco_customer_churn.exception import CustomException
 if __name__=='__main__':
     logging.info('hi')
+    try:
+        a=10/0
+    except Exception as e:
+        raise CustomException(e,sys)
